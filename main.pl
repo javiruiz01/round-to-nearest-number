facfc51b912819f071536eb22bcdb015ca01e812
Javier Ruiz <jarc0212@gmail.com>
2018-04-23 15:32:53 +0200

    Define Peano method, begin parsing arguments

--- main.pl
+++ main.pl
@@ -2,3 +2,23 @@
 % Javier Ruiz Calle
 
 :- module(_,_).
+redondearDecimal(NumeroInicial, TipoRedondeo, NumeroFinal):-
+    leerTipo(TipoRedondeo, NumeroInicial, NumeroFinal).
+
+leerTipo(redondeoUnidad, NumeroInicial, NumeroFinal):-
+    recorrerParteEntera(NumeroInicial, NumeroFinal).
+
+leerTipo(redondeoDecimas, NumeroInicial, NumeroFinal):-.
+
+leerTipo(redondeoCentesimas, NumeroInicial, NumeroFinal):-.
+
+recorrerParteEntera([',' | T], NumeroFinal):-
+    recorrerParteDecimal().
+recorrerParteEntera([X | T], NumeroFinal):-
+    X \= ',',
+    recorrerParteEntera(T, NumeroFinal).
+
+% Métodos Peano
+peano_add(0, N, N).
+peano_add( s(N), M, s(Sum) ) :-
+	peano_add( N, M, Sum ).
