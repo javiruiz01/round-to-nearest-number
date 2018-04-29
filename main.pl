f8710c9a260febdf984089dfe70a19f6d6cb186a
Javier Ruiz <jarc0212@gmail.com>
2018-04-29 16:25:34 +0200

    Round up to decimals without worrying about carry input

--- main.pl
+++ main.pl
@@ -20,9 +20,7 @@
 
 :- module(_,_).
 
-numeroFinal(Separador, ParteEnteraF, ParteDecimalF).
-
-redondearDecimal(NumeroInicial, TipoRedondeo, NumeroFinal):-
+redondearDecimal(NumeroInicial, TipoRedondeo, NumeroFinal) :-
     recorrerParteEntera(TipoRedondeo, NumeroInicial, [], NumeroFinal).
 
 % Recorremos parte entera y se va guardando en una lista
@@ -31,7 +29,7 @@ recorrerParteEntera(TipoRedondeo, [',' | T], ParteEntera, NumeroFinal):-
     recorrerParteDecimal(TipoRedondeo, T, ParteEntera, [], ',',NumeroFinal).
 recorrerParteEntera(TipoRedondeo, [X | T], ParteEntera ,NumeroFinal):-
     X \= ',',
-    append(ParteEntera, [X],  Z),
+    my_append(ParteEntera, [X],  Z),
     recorrerParteEntera(TipoRedondeo, T, Z, NumeroFinal).
 
 % Recorremos la parte decimal y se guarda en una lista,
@@ -39,50 +37,28 @@ recorrerParteEntera(TipoRedondeo, [X | T], ParteEntera ,NumeroFinal):-
 recorrerParteDecimal(TipoRedondeo, [], ParteEntera, ParteDecimal, Separador, NumeroFinal):-
     construirNumeroFinal(redondeo(TipoRedondeo, numeroOriginal(Separador, ParteEntera, ParteDecimal), numeroFinal(Separador, [], []))).
 recorrerParteDecimal(TipoRedondeo, [X | T], ParteEntera, ParteDecimal, Separador, NumeroFinal):-
-    append(ParteDecimal, [X],  Z),
+    my_append(ParteDecimal, [X],  Z),
     recorrerParteDecimal(TipoRedondeo, T, ParteEntera, Z, Separador, NumeroFinal).
 
 construirNumeroFinal(redondeo(redondeoUnidad, numeroOriginal(Separador, ParteEnteraO, ParteDecimalO), numeroFinal(Separador, ParteEnteraF, ParteDecimalF))).
 
 construirNumeroFinal(redondeo(redondeoDecimas, numeroOriginal(Separador, ParteEnteraO, [ X, Y | _ ]), numeroFinal(Separador, ParteEnteraF, ParteDecimalF))):-
-    % Uso de less or equals para saber si tenemos que redondear o no
-    redondear(X, Y, Salida),
-    reverse(Salida, Z), % Porque el append añade por delante
-    comprobarAcarreoParteDecimal(Z, SalidaDecimal),
-    reverse(ParteEnteraO, W),
-    comprobarAcarreoParteEntera(W, Z, SalidaEntera),
-    reverse(Z, Zs),
-    reverse(SalidaEntera, Ws),
-    numeroFinal(Separador, Ws, Zs).
+    redondearParteDecimal(ParteEnteraO, X, Y, Decimal),
+    my_append(ParteEnteraF, ParteEnteraO, Z),
+    my_append(ParteDecimalF, [Decimal], Zs).
 
-redondear(Elemento, Referencia, Salida) :-
+redondearParteDecimal(ParteEnteraO, Elemento, Referencia, Salida) :-
     less_or_equal(Referencia, s(s(s(s(0))))),
-    peano_add(Elemento, 0, Z),
-    append([Z], [], Salida).
-redondear(Elemento, Referencia, Salida) :-
-    peano_add(Elemento, s(0), Z),
-    append([Z], [], Salida).
+    peano_add(Elemento, 0, Z).
+redondearParteDecimal(ParteEnteraO, Elemento, Referencia, Salida) :-
+    less_or_equal(s(s(s(s(0)))), Referencia),
+    peano_add(Elemento, s(0), Salida).
 
-comprobarAcarreoParteDecimal([X | T], Salida):-
-    X = s(s(s(s(s(s(s(s(s(s(0)))))))))),
-    suma(T, Salida),
-    comprobarAcarreoParteDecimal(T, Salida).
-comprobarAcarreoParteDecimal([X | T], Salida):-
-    peano_add(X, 0, Z),
-    comprobarAcarreoParteDecimal(T, Z).
-
-comprobarAcarreoParteEntera([X | T], [Y | _], SalidaEntera) :-
-    X = s(s(s(s(s(s(s(s(s(s(0)))))))))),
-    suma(T, SalidaEntera),
-    comprobarAcarreoParteEntera(SalidaEntera, Y, SalidaEntera).
-
-suma([X | T], Salida):-
+suma([X | T], Salida) :-
     peano_add(X, s(0), Z),
-    append([Z], T, Salida).
+    my_append([Z], T, Salida).
 
 construirNumeroFinal(redondeo(redondeoCentesimas, numeroOriginal(Separador, ParteEnteraO, ParteDecimalO), numeroFinal(Separador, ParteEnteraF, ParteDecimalF))).
-
-% recorrerParteEntera(redondeoUnidad, [s(0),',',s(s(s(0)))], [], []).
 
 % Métodos auxiliares
 less_or_equal(0,X) :-
@@ -94,10 +70,14 @@ nat(0).
 nat(s(X)) :-
     nat(X).
 
-append([], Y, Y).
-append([H|X], Y, [H|Z]) :-
-    append(X, Y, Z).
+my_append([], Y, Y).
+my_append([H|X], Y, [H|Z]) :-
+    my_append(X, Y, Z).
 
 peano_add(0, N, N).
 peano_add( s(N), M, s(Sum) ) :-
 	peano_add( N, M, Sum ).
+
+% Ejemplo
+% redondearDecimal([s(s(s(s(s(0))))),',',s(s(s(0)))], redondeoUnidad, redondeo(redondeoUnidad, numeroOriginal(',', [s(s(s(s(s(0)))))], [s(s(s(0)))]), numeroRedondeado(',', [s(s(s(s(s(0)))))], []))).
+% redondearDecimal([s(0), ',', s(s(s(s(s(s(s(0))))))), s(s(s(s(s(s(s(s(0))))))))], redondeoDecimas, X).
